8e57e14d90eabd321d91a24d62e617edabab03bb
meh <meh@schizofreni.co>
2014-05-11 21:11:03 +0200

    mix: bump version and requirements

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule Dexts.Mixfile do
 
   def project do
     [ app: :dexts,
-      version: "0.1.1-dev",
-      elixir: "~> 0.13.0",
+      version: "0.1.1",
+      elixir: "~> 0.13.2",
       package: package,
       description: "dets wrapper for Elixir" ]
   end
 
   defp package do
     [ contributors: ["meh"],
-      licenses: ["WTFPL"],
+      license: "WTFPL",
       links: [ { "GitHub", "https://github.com/meh/dexts" } ] ]
   end
 end
