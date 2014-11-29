0e87d691609c95544aaf7ae3a5187c5ae503cf68
kq <kq@qsc.de>
2014-11-29 21:14:42 +0100

    mix: fixed warning for elixir version

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule Dexts.Mixfile do
   def project do
     [ app: :dexts,
       version: "0.2.0",
-      elixir: "~> 0.14.3",
+      elixir: "~> 1.0.0",
       package: package,
       description: "dets wrapper for Elixir" ]
   end
 
   defp package do
-    [ contributors: ["meh"],
+    [ contributors: ["meh", "cereal"],
       licenses: ["WTFPL"],
       links: [ { "GitHub", "https://github.com/meh/dexts" } ] ]
   end
